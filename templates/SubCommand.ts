f00a63cfc9d63fb0b638ad10ee8ef7d2e4014c9a
TSLARoadster <51007646+TSLARoadster@users.noreply.github.com>
2022-07-08 16:46:11 -0400

    Fix SubCommand template

--- templates/SubCommand.ts
+++ templates/SubCommand.ts
@@ -1,23 +1,31 @@
+import { CommandInteraction } from 'discord.js'
+
 /**
  * Represents a SubCommand
  */
 export default class SubCommand {
-    execute: Function
+    execute: (interaction: CommandInteraction) => Promise<void> | void
 
     /**
-     * 
+     *
      * @param {{
      *      execute: Function
      *  }} options - The options for the subcommand
      */
-    constructor(options: {execute: Function}) {
+    constructor(options: {
+        execute: (interaction: CommandInteraction) => Promise<void> | void
+    }) {
         this.execute = options.execute
     }
 
     /**
-     * @param {Function} executeFunction - The function
+     * @param {(interaction: CommandInteraction) => Promise<void> | void} executeFunction - The function
      */
-    setExecute(executeFunction: Function): void {
+    setExecute(
+        executeFunction: (
+            interaction: CommandInteraction
+        ) => Promise<void> | void
+    ): void {
         this.execute = executeFunction
     }
 }
