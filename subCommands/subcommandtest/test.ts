eef22e5606674f1cb3b9212b9ad5fc10af27e5c5
TSLARoadster <51007646+TSLARoadster@users.noreply.github.com>
2022-07-08 16:47:26 -0400

    Fix SubCommand test

--- subCommands/subcommandtest/test.ts
+++ subCommands/subcommandtest/test.ts
@@ -1,10 +1,12 @@
-const SubCommand = require('../../templates/SubCommand')
+import SubCommand from '../../templates/SubCommand'
 
 module.exports = new SubCommand({
     async execute(interaction) {
         const input = interaction.options.get('input')?.value ?? false
 
-        const response = input ? `You inputted: ${input}` : "You didn't input anything"
+        const response = input
+            ? `You inputted: ${input}`
+            : "You didn't input anything"
 
         interaction.reply(response)
     },
